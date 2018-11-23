79f8095131b9050c36e5e39f380bad6233c508cf
matthinc <matthias.rupp@posteo.de>
2018-11-23 15:21:20 +0100

    Switch to dialog-based model for user creation

--- src/main/java/promillerechner/handlers/CreateUserIntentHandler.java
+++ src/main/java/promillerechner/handlers/CreateUserIntentHandler.java
@@ -2,8 +2,12 @@ package main.java.promillerechner.handlers;
 
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.DialogState;
+import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
@@ -16,6 +20,21 @@ public class CreateUserIntentHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
-        return handlerInput.getResponseBuilder().withSpeech("Hallo, Welt!").build();
+        IntentRequest request = (IntentRequest) handlerInput.getRequestEnvelope().getRequest();
+        if (request.getDialogState() == DialogState.COMPLETED) {
+
+            //Map<String, Slot> slots = request.getIntent().getSlots();
+
+            return handlerInput
+                    .getResponseBuilder()
+                    .withSpeech("Ok, ich habe den Benutzer angelegt.")
+                    .build();
+        } else {
+            return handlerInput
+                    .getResponseBuilder()
+                    .addDelegateDirective(request.getIntent())
+                    .build();
+        }
+
     }
 }
