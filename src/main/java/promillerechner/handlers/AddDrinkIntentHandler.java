cb8d586176d9c039d1c603c250487010013a00c3
Draqu Underworld <joemungandar95@gmail.com>
2018-12-06 13:10:18 +0100

    Update AddDrinkIntentHandler

--- src/main/java/promillerechner/handlers/AddDrinkIntentHandler.java
+++ src/main/java/promillerechner/handlers/AddDrinkIntentHandler.java
@@ -1,10 +1,13 @@
 package promillerechner.handlers;
 
+import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
 import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Response;
 import promillerechner.Constants;
 
+import java.util.Map;
 import java.util.Optional;
 
 import static com.amazon.ask.request.Predicates.intentName;
@@ -17,6 +20,11 @@ public class AddDrinkIntentHandler implements RequestHandler {
 
     @Override
     public Optional<Response> handle(HandlerInput handlerInput) {
+
+        AttributesManager attributesManager = handlerInput.getAttributesManager();
+        Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
+
+        IntentRequest request = (IntentRequest) handlerInput.getRequestEnvelope().getRequest();
         return Optional.empty();
     }
 }
