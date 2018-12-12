75cbb36af7225b43d2ed44ac7f8873eaf5fc75b5
Draqu Underworld <joemungandar95@gmail.com>
2018-12-12 20:47:28 +0100

    AddDrinkIntentHandler 3. Testfall aufgestellt

--- src/test/java/AddDrinkIntentHandlerTest.java
+++ src/test/java/AddDrinkIntentHandlerTest.java
@@ -1,21 +1,33 @@
 
 import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
-import org.junit.jupiter.api.Test;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
+import promillerechner.Constants;
+import promillerechner.ToolsTest;
 import promillerechner.handlers.AddDrinkIntentHandler;
+import promillerechner.model.User;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class AddDrinkIntentHandlerTest {
 
-    AddDrinkIntentHandler handler = new AddDrinkIntentHandler();
+    AddDrinkIntentHandler handler;
     HandlerInput handlerInput = mock(HandlerInput.class);
+
+    @Before
+    public void setup() {
+        handler = new AddDrinkIntentHandler();
+    }
 
     @Test
     public void testCanHandle() {
@@ -26,15 +38,37 @@ public class AddDrinkIntentHandlerTest {
 
     @Test
     public void testAddADrinkToAUser() {
-        final AttributesManager coustomAttributesmanager = Mockito.mock(AttributesManager.class);
+        final AttributesManager customAttributesmanager = Mockito.mock(AttributesManager.class);
         Map<String, Slot> data = new HashMap<>();
+        Map<String, Object> persistentAttributes = new HashMap<>();
 
         //Testbelegung:
-        data.put("name",Slot.builder().withName("name").withValue("benjamin").build());
-        data.put("age",Slot.builder().withName("age").withValue("22").build());
-        data.put("sex",Slot.builder().withName("sex").withValue("Männlich").build());
-        data.put("mass",Slot.builder().withName("mass").withValue("75").build());
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "benjamin");
+        user.put("age", 22);
+        user.put("sex", "Männlich");
+        user.put("mass", 75);
 
+        HandlerInput testHandlerInput = ToolsTest.coustemHandlerInput(customAttributesmanager, data);
 
+        System.out.println("Current User: " + User.getCurrentUser(customAttributesmanager));
+        final Optional<Response> res = handler.handle(testHandlerInput);
+        assertTrue(res.isPresent());
+
+        final Response response = res.get();
+        System.out.println(response.getOutputSpeech().toString());
+    }
+
+    @Test
+    public void testAddDrinkWhenNoUserIsSelected() {
+        final AttributesManager customAttributesmanager = Mockito.mock(AttributesManager.class);
+        Map<String, Slot> data = new HashMap<>();
+
+        HandlerInput testHandlerInput = ToolsTest.coustemHandlerInput(customAttributesmanager, data);
+        final Optional<Response> res = handler.handle(testHandlerInput);
+        assertTrue(res.isPresent());
+
+        final Response response = res.get();
+        assertTrue(response.getOutputSpeech().toString().contains(Constants.ADD_DRINK_NO_VALID_USER_ERROR));
     }
 }
