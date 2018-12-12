a3cd082d6f3375676ca69de13b174cce5847ccac
Draqu Underworld <joemungandar95@gmail.com>
2018-12-12 19:20:00 +0100

    AddDrinkIntentHandler + Constants angepasst, 2. Testfall aufgestellt (unvollständig)

--- src/test/java/AddDrinkIntentHandlerTest.java
+++ src/test/java/AddDrinkIntentHandlerTest.java
@@ -1,8 +1,13 @@
 
+import com.amazon.ask.attributes.AttributesManager;
 import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.model.Slot;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import promillerechner.handlers.AddDrinkIntentHandler;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -21,6 +26,15 @@ public class AddDrinkIntentHandlerTest {
 
     @Test
     public void testAddADrinkToAUser() {
+        final AttributesManager coustomAttributesmanager = Mockito.mock(AttributesManager.class);
+        Map<String, Slot> data = new HashMap<>();
+
+        //Testbelegung:
+        data.put("name",Slot.builder().withName("name").withValue("benjamin").build());
+        data.put("age",Slot.builder().withName("age").withValue("22").build());
+        data.put("sex",Slot.builder().withName("sex").withValue("Männlich").build());
+        data.put("mass",Slot.builder().withName("mass").withValue("75").build());
+
 
     }
 }
