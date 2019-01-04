d46d0b4f1f2f74a65bb268e4038e9d0539965ce0
Jan <bremauer.jan2@gmai.com>
2019-01-04 12:29:45 +0100

    added promilledepleting

--- src/main/java/promillerechner/handlers/PromilleDepletingIntentHandler.java
+++ src/main/java/promillerechner/handlers/PromilleDepletingIntentHandler.java
@@ -8,6 +8,7 @@ import com.amazon.ask.model.IntentRequest;
 import com.amazon.ask.model.Response;
 import com.amazon.ask.model.Slot;
 import promillerechner.Constants;
+import promillerechner.model.User;
 
 import java.util.Map;
 import java.util.Optional;
@@ -15,37 +16,18 @@ import java.util.Optional;
 import static com.amazon.ask.request.Predicates.intentName;
 
 public class PromilleDepletingIntentHandler implements RequestHandler {
+
     @Override
     public boolean canHandle(HandlerInput handlerInput) {
         return handlerInput.matches(intentName(Constants.INTENT_DEPLET_PROMILLEVALUE));
     }
 
     @Override
-    public Optional<Response> handle(HandlerInput handlerInput) {
-
-        AttributesManager attributesManager = handlerInput.getAttributesManager();
-        IntentRequest request = (IntentRequest) handlerInput.getRequestEnvelope().getRequest();
-
-        if (request.getDialogState() == DialogState.COMPLETED) {
-            boolean gotValue = false;
-            Map<String, Slot> slots = request.getIntent().getSlots();
-
-            if (gotValue) {
-                return handlerInput
-                        .getResponseBuilder()
-                        .withSpeech(Constants.REMOVE_USER_TEXT)
-                        .build();
-            } else {
-                return handlerInput
-                        .getResponseBuilder()
-                        .withSpeech(Constants.REMOVE_USER_ERROR)
-                        .build();
-            }
-        } else {
-            return handlerInput
-                    .getResponseBuilder()
-                    .addDelegateDirective(request.getIntent())
-                    .build();
-        }
+    public Optional<Response> handle(HandlerInput input) {
+        float promille = User.readPromille(input.getAttributesManager());
+        return input.getResponseBuilder()
+                .withSpeech("Du bist in ungefähr " + (int)(promille/0.15) + " Stunden wieder nüchtern!")
+                .withShouldEndSession(false)
+                .build();
     }
 }
