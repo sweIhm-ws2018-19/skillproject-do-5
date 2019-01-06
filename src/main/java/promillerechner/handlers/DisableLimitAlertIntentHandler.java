84f6f56da3f3b4b89f44f7274d2a73186ea45e94
seanog8 <obenhack@hm.edu>
2019-01-06 15:37:46 +0100

    add DisableLimitAlertIntentHandler

---
+++ src/main/java/promillerechner/handlers/DisableLimitAlertIntentHandler.java
@@ -0,0 +1,42 @@
+package promillerechner.handlers;
+
+import com.amazon.ask.attributes.AttributesManager;
+import com.amazon.ask.dispatcher.request.handler.HandlerInput;
+import com.amazon.ask.dispatcher.request.handler.RequestHandler;
+import com.amazon.ask.model.DialogState;
+import com.amazon.ask.model.IntentRequest;
+import com.amazon.ask.model.Response;
+import com.amazon.ask.model.Slot;
+import com.amazon.ask.response.ResponseBuilder;
+import promillerechner.Constants;
+import promillerechner.model.Drink;
+import promillerechner.model.User;
+
+import java.util.Map;
+import java.util.Optional;
+
+import static com.amazon.ask.request.Predicates.intentName;
+
+public class DisableLimitAlertIntentHandler implements RequestHandler {
+    @Override
+    public boolean canHandle(HandlerInput handlerInput) {
+        return handlerInput.matches(intentName(Constants.INTENT_ENABLE_LIMIT));
+    }
+
+
+    @Override
+    public Optional<Response> handle(HandlerInput handlerInput) {
+        AttributesManager attributesManager = handlerInput.getAttributesManager();
+
+        Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
+        persistentAttributes.put(Constants.LIMITALERT, false);
+
+        attributesManager.savePersistentAttributes();
+
+        return handlerInput.getResponseBuilder()
+                .withSpeech("Alkoholpegelwarnung deaktiviert")
+                .withShouldEndSession(false)
+                .build();
+    }
+
+}
