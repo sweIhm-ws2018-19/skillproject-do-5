cab4f0f317a2c2c9c7fac443d4db574e8650c6a8
seanog8 <obenhack@hm.edu>
2019-01-06 15:38:24 +0100

    add LimitAlert class

---
+++ src/main/java/promillerechner/calculations/LimitAlert.java
@@ -0,0 +1,35 @@
+package promillerechner.calculations;
+
+import com.amazon.ask.attributes.AttributesManager;
+import promillerechner.Constants;
+import promillerechner.model.User;
+
+import java.util.Map;
+
+public class LimitAlert {
+
+    public static String getAlertMessageDependentByAge(AttributesManager attributesManager) {
+        Map<String, Object> persistentAttributes = attributesManager.getPersistentAttributes();
+        persistentAttributes.putIfAbsent(Constants.LIMITALERT, false);
+        String returnMessage = "";
+        if ((boolean)persistentAttributes.get(Constants.LIMITALERT)) {
+            User current =(User)persistentAttributes.get(Constants.CURRENTUSER);
+            int age = current.getAge();
+            float promille = User.readPromille(attributesManager);
+            if (promille > 2) {
+                returnMessage = "Egal wie alt du bist, du solltest jetzt aufhören Alkohol zu trinken!";
+            } else if (age < 16 && promille > 0.1f) {
+                returnMessage = "Du hast schon " + promille + "Promille. Ich werde deine Eltern benachrichtigen!";
+            } else if (age < 18 && promille > 0.5f) {
+                returnMessage = promille + "Promille. Trinkst du jetzt weiter könntest du deinen Gehirnzellen schaden!" +
+                    " Nicht das du den ganzen Stoff aus der Schule verlierst!";
+            } else if (age < 21 && promille > 1) {
+                returnMessage = promille + "Promille. Jetzt langt's aber!";
+            } else if (promille > 1.5f) {
+                returnMessage = promille + "Promille. Geht es dir nicht gut? Trinkst du deshalb soviel? Ich bin hier zum Zuhören" +
+                        "falls du jemand zum reden brauchst!";
+            }
+        }
+        return returnMessage;
+    }
+}
