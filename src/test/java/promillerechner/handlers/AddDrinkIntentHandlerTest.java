072ffc740a1b6329e6070cebd0316110050e3b00
Draqu Underworld <joemungandar95@gmail.com>
2019-01-07 18:49:45 +0100

    Added Test "AddNotAValidDrink" From Branch to master

--- src/test/java/promillerechner/handlers/AddDrinkIntentHandlerTest.java
+++ src/test/java/promillerechner/handlers/AddDrinkIntentHandlerTest.java
@@ -102,6 +102,78 @@ public class AddDrinkIntentHandlerTest {
     }
 
     @Test
+    public void testAddNotAValidDrink() {
+        Map<String, Slot> data = new HashMap<>();
+        Map<String, Object> persistentAttributes = new HashMap<>();
+        Map<String, Object> user = new HashMap<>();
+        List<Map<String, Object>> userList = new LinkedList<>();
+        Map<String, Object> currentUser = new HashMap<>();
+        Map<String, Object> drink = new HashMap<>();
+        List<Map<String, Object>> drinkList = new ArrayList<>();
+
+        data.put("drinks", Slot.builder().withName("drinks").withValue("KETCHUP").build());
+        data.put("container", Slot.builder().withName("container").withValue("FLASCHE").build());
+
+        //Testbelegung:
+        user.put("name", "benjamin");
+        user.put("age", new BigDecimal(22));
+        user.put("sex", "Männlich");
+        user.put("mass", new BigDecimal(75));
+        currentUser.put("name", "benjamin");
+        currentUser.put("age", new BigDecimal(22));
+        currentUser.put("sex", "Männlich");
+        currentUser.put("mass", new BigDecimal(75));
+        drink.put("name", "KETCHUP");
+        drink.put("container", "FLASCHE");
+        drink.put("user", "benjamin");
+
+        drinkList.add(drink);
+        userList.add(user);
+        persistentAttributes.put("users", userList);
+        persistentAttributes.put("currentUser", currentUser);
+
+        //Danach
+        Map<String, Object> userOutput = new HashMap<>();
+        List<Map<String, Object>> userOutputList = new LinkedList<>();
+        Map<String, Object> currentUserOutput = new HashMap<>();
+        Map<String, Object> drinkOutput = new HashMap<>();
+        List<Map<String, Object>> drinkListOutput = new ArrayList<>();
+        Map<String, Object> outputAttributes = new HashMap<>();
+        userOutput.put("name", "benjamin");
+        userOutput.put("age", new BigDecimal(22));
+        userOutput.put("sex", "Männlich");
+        userOutput.put("mass", new BigDecimal(75));
+        currentUserOutput.put("name", "benjamin");
+        currentUserOutput.put("age", new BigDecimal(22));
+        currentUserOutput.put("sex", "Männlich");
+        currentUserOutput.put("mass", new BigDecimal(75));
+        drinkOutput.put("name", "WEIN");
+        drinkOutput.put("container", "MASS");
+        drinkOutput.put("user", "benjamin");
+        drinkOutput.put("date", new Date().toString());
+        drinkListOutput.add(drinkOutput);
+        userOutputList.add(userOutput);
+        outputAttributes.put("currentUser", currentUserOutput);
+        outputAttributes.put("drinks", drinkListOutput);
+        outputAttributes.put("users", userOutputList);
+
+        // Mock attributesManager
+        final AttributesManager customAttributesmanager = Mockito.mock(AttributesManager.class);
+        when(customAttributesmanager.getPersistentAttributes()).thenReturn(persistentAttributes);
+
+        // Mock setPersistentttributes and capture input
+        ArgumentCaptor<Map<String, Object>> arg = ArgumentCaptor.forClass(Map.class);
+        doNothing().when(customAttributesmanager).setPersistentAttributes(arg.capture());
+
+        HandlerInput test = ToolsTest.custemHandlerInput(customAttributesmanager, data);
+        final Optional<Response> res = handler.handle(test);
+        assertTrue(res.isPresent());
+        final Response response = res.get();
+
+        assertTrue(response.getOutputSpeech().toString().contains(Constants.NO_VALID_DRINK_ERROR));
+    }
+
+    @Test
     public void testAddADrinkToAUserWithWrongContainer() {
         Map<String, Slot> data = new HashMap<>();
         Map<String, Object> persistentAttributes = new HashMap<>();
